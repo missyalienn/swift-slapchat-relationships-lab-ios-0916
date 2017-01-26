cd7502f23932552c3943ff4281606a0199c5d702
missyalienn <allan.missy@gmail.com>
2017-01-26 16:52:30 -0500

    looks like i forgot to commit this

--- SlapChat/TableViewController.swift
+++ SlapChat/TableViewController.swift
@@ -14,6 +14,7 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        store.generateTestData()
         store.fetchData()
     }
     
